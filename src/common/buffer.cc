04b046afed383b1e1873c8b06b42cdc41af025ae
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-10-08 14:59:41 +0300

    Fix Windows platform checks
    Some code is conditionally compiled for __CYGWIN__. Let's also
    check for _WIN32, allowing cross compilation from Linux hosts
    or even using clang/VC on Windows at some point.

--- src/common/buffer.cc
+++ src/common/buffer.cc
@@ -157,7 +157,7 @@ static ceph::spinlock debug_lock;
     }
   };
 
-#ifndef __CYGWIN__
+#if !defined(__CYGWIN__) && !defined(_WIN32)
   class buffer::raw_posix_aligned : public buffer::raw {
     unsigned align;
   public:
@@ -188,7 +188,7 @@ static ceph::spinlock debug_lock;
   };
 #endif
 
-#ifdef __CYGWIN__
+#if defined(__CYGWIN__) || defined(_WIN32)
   class buffer::raw_hack_aligned : public buffer::raw {
     unsigned align;
     char *realdata;
@@ -344,7 +344,7 @@ static ceph::spinlock debug_lock;
     // size passes 8KB.
     if ((align & ~CEPH_PAGE_MASK) == 0 ||
 	len >= CEPH_PAGE_SIZE * 2) {
-#ifndef __CYGWIN__
+#if !defined(__CYGWIN__) && !defined(_WIN32)
       return ceph::unique_leakable_ptr<buffer::raw>(new raw_posix_aligned(len, align));
 #else
       return ceph::unique_leakable_ptr<buffer::raw>(new raw_hack_aligned(len, align));
