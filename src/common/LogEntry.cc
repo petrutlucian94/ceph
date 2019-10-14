15c6afccb5b1c1bade9d9e9eba8fd9947d013f50
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-10-14 19:28:38 +0300

    [squashme][win32]LogEntry.cc: only use syslog if available
    syslog is not available on Windows, for which reason we'll need
    some platform checks.

--- src/common/LogEntry.cc
+++ src/common/LogEntry.cc
@@ -1,7 +1,15 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 //
-#include <syslog.h>
+
+#if !defined(_WIN32)
+  #include <syslog.h>
+
+  #define SYSLOG_AVAILABLE 1
+#else
+  #define SYSLOG_AVAILABLE 0
+#endif
+
 #include <boost/algorithm/string/predicate.hpp>
 
 #include "LogEntry.h"
@@ -47,6 +55,7 @@ clog_type LogEntry::str_to_level(std::string const &str)
 
 // ----
 
+#if SYSLOG_AVAILABLE
 int clog_type_to_syslog_level(clog_type t)
 {
   switch (t) {
@@ -65,6 +74,7 @@ int clog_type_to_syslog_level(clog_type t)
       return 0;
   }
 }
+#endif
 
 clog_type string_to_clog_type(const string& s)
 {
@@ -88,6 +98,7 @@ clog_type string_to_clog_type(const string& s)
   return CLOG_UNKNOWN;
 }
 
+#if SYSLOG_AVAILABLE
 int string_to_syslog_level(string s)
 {
   if (boost::iequals(s, "debug"))
@@ -156,6 +167,7 @@ int string_to_syslog_facility(string s)
   // default to USER
   return LOG_USER;
 }
+#endif
 
 string clog_type_to_string(clog_type t)
 {
@@ -178,6 +190,7 @@ string clog_type_to_string(clog_type t)
 
 void LogEntry::log_to_syslog(string level, string facility)
 {
+  #if SYSLOG_AVAILABLE
   int min = string_to_syslog_level(level);
   int l = clog_type_to_syslog_level(prio);
   if (l <= min) {
@@ -188,6 +201,7 @@ void LogEntry::log_to_syslog(string level, string facility)
 	   (long long unsigned)seq,
 	   msg.c_str());
   }
+  #endif
 }
 
 void LogEntry::encode(bufferlist& bl, uint64_t features) const
