fd1d943e6b958f7e198c5ea831a71cceba7592ef
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-10-16 16:14:52 +0300

    [win32] ensure unix sockets are available
    Unix sockets are available on Windows starting with the
    17063 SDK. sockaddr_un is defined by afunix.h instead of sys/un.h
    and it's not included by mingw for the time being, for which
    reason we're going to define it.

--- src/include/sock_types.h
+++ src/include/sock_types.h
@@ -19,6 +19,21 @@
 #define poll WSAPoll
 #endif
 
+// afunix.h is available starting with Windows SDK 17063. Still, it wasn't
+// picked up by mingw yet, for which reason we're going to define sockaddr_un
+// here.
+#ifndef _AFUNIX_
+#define UNIX_PATH_MAX 108
+
+typedef struct sockaddr_un
+{
+     ADDRESS_FAMILY sun_family;     /* AF_UNIX */
+     char sun_path[UNIX_PATH_MAX];  /* pathname */
+} SOCKADDR_UN, *PSOCKADDR_UN;
+
+#define SIO_AF_UNIX_GETPEERPID _WSAIOR(IOC_VENDOR, 256)
+#endif /* _AFUNIX */
+
 #else
 #include <netdb.h>
 #include <sys/socket.h>
