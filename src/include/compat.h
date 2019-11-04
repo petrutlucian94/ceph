233cf43937c3682d7967933facbe46fca000739e
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-11-04 08:49:31 +0000

    [win32] Add missing type and constant definitions
    The following types and constants are not defined on Windows, for
    which reason we're going to include them in compat.h.
    
    * uid_t, gid_t
    * sigset_t
    * HOST_NAME_MAX
    * O_CLOEXEC, which we're going to define as a no-op. By default,
      handles are not inherited by subprocesses.
    * SHUT_* flags
    * loff_t
    * __STRING
    * EBADE, ENODATA, ESHUTDOWN, ESTALE, EREMOTEIO
    * suseconds_t
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- src/include/compat.h
+++ src/include/compat.h
@@ -103,15 +103,15 @@ int sched_setaffinity(pid_t pid, size_t cpusetsize,
 #define XATTR_CREATE 1
 #endif
 
+#endif /* __APPLE__ */
+
 #ifndef HOST_NAME_MAX
 #ifdef MAXHOSTNAMELEN 
 #define HOST_NAME_MAX MAXHOSTNAMELEN 
 #else
 #define HOST_NAME_MAX 255
 #endif
-#endif
-
-#endif /* __APPLE__ */
+#endif /* HOST_NAME_MAX */
 
 /* O_LARGEFILE is not defined/required on OSX/FreeBSD */
 #ifndef O_LARGEFILE
@@ -204,5 +204,39 @@ char *ceph_strerror_r(int errnum, char *buf, size_t buflen);
 #ifdef __cplusplus
 }
 #endif
+
+// TODO: determine when running cmake
+#if defined(_WIN32)
+
+typedef _sigset_t sigset_t;
+
+typedef int uid_t;
+typedef int gid_t;
+
+typedef long blksize_t;
+typedef long blkcnt_t;
+typedef long nlink_t;
+
+typedef long long loff_t;
+
+#define SHUT_RD SD_RECEIVE
+#define SHUT_WR SD_SEND
+#define SHUT_RDWR SD_BOTH
+
+#ifndef ENODATA
+// mingw doesn't define this, the Windows SDK does.
+#define ENODATA 120
+#endif
+
+#define ESHUTDOWN ECONNABORTED
+#define ESTALE 256
+#define EREMOTEIO 257
+
+// O_CLOEXEC is not defined on Windows. Since handles aren't inherited
+// with subprocesses unless explicitly requested, we'll define this
+// flag as a no-op.
+#define O_CLOEXEC 0
+
+#endif /* WIN32 */
 
 #endif /* !CEPH_COMPAT_H */
