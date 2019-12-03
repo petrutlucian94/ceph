a8dc3dad863eca05d209dc6e496465d654428276
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-12-03 12:30:41 +0000

    [win32] Use sockets instead of pipes for wakeup events
    On Windows select (WSASelct) as well as other similar socket APIs
    (e.g. WSAPoll) can only be used with socket handles, while some
    loops are also using pips for wakeup events.
    
    On Windows, we'll have to replace those with socket pairs.
    
    Note that WaitForMultipleObjects accepts various types of handles
    (pipes, events, thread/process handles) but not sockets.
    
    At the same time, the return codes are slightly different, besides
    just having the WSA prefix. For example, for non-blocking sockets
    we'd get WSAEWOULDBLOCK instead of EAGAIN. Also, those must
    be retrieved using WSAGetLastError() instead of errno, for which
    we're defining the ceph_sock_errno helper.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- src/include/compat.h
+++ src/include/compat.h
@@ -14,6 +14,7 @@
 
 #include "acconfig.h"
 #include <sys/types.h>
+#include <errno.h>
 
 #if defined(__linux__)
 #define PROCPREFIX
@@ -290,6 +291,8 @@ int chown(const char *path, uid_t owner, gid_t group);
 int fchown(int fd, uid_t owner, gid_t group);
 int lchown(const char *path, uid_t owner, gid_t group);
 
+int win_socketpair(int socks[2]);
+
 #ifdef __cplusplus
 }
 
@@ -342,5 +345,14 @@ int lchown(const char *path, uid_t owner, gid_t group);
 #else
 #define CEPH_PACKED(DECL) __pragma(pack(push, 1)) DECL __pragma(pack(pop))
 #endif
+
+/* This should only be used with the socket API. */
+static inline int ceph_sock_errno() {
+#ifdef _WIN32
+  return WSAGetLastError();
+#else
+  return errno;
+#endif
+}
 
 #endif /* !CEPH_COMPAT_H */
