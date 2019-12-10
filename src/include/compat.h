ea251980a7c7dea57a3e2d06d4bd788c28e034ec
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-12-10 14:16:09 +0000

    [win32] Define aligned_free
    Memory allocated using _aligned_malloc (replacement for posix_malloc)
    has to be deallocated by _aligned_free, otherwise leading to a crash.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- src/include/compat.h
+++ src/include/compat.h
@@ -300,6 +300,9 @@ int lchown(const char *path, uid_t owner, gid_t group);
 
 // Windows' mkdir doesn't accept a mode argument.
 #define compat_mkdir(pathname, mode) mkdir(pathname)
+// Use "aligned_free" when freeing memory allocated using posix_memalign or
+// _aligned_malloc. Using "free" will crash.
+#define aligned_free(ptr) _aligned_free(ptr)
 
 #endif
 
@@ -309,11 +312,16 @@ int lchown(const char *path, uid_t owner, gid_t group);
 #define O_CLOEXEC 0
 #define SOCKOPT_VAL_TYPE char*
 
-#else
+#else /* WIN32 */
 
 #define SOCKOPT_VAL_TYPE void*
 
+<<<<<<< HEAD
 #define compat_mkdir(pathname, mode) mkdir(pathname, mode)
+=======
+#define rpl_mkdir(pathname, mode) mkdir(pathname, mode)
+#define aligned_free(ptr) free(ptr)
+>>>>>>> 100231877b... [win32] Define aligned_free
 
 #endif /* WIN32 */
 
