77aeaf7200d26141a0bc2c6883a1612526c6cc7e
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2019-12-19 13:21:49 +0000

    [win32] port Ceph tests
    We're porting to Windows some of the Ceph tests, mostly related to rados
    and rbd.
    
    Overall, this is about using compat.h functions and types as well as avoiding
    unsupported functionality.
    
    The systest framework will have to be limitted to threads instead of separate
    processes until we cover the process spawning and communication.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- src/test/librados/misc.cc
+++ src/test/librados/misc.cc
@@ -17,7 +17,9 @@
 #include "test/librados/TestCase.h"
 #include "gtest/gtest.h"
 #include <sys/time.h>
+#ifndef _WIN32
 #include <sys/resource.h>
+#endif
 
 #include <errno.h>
 #include <map>
@@ -323,6 +325,7 @@ static void shutdown_racer_func()
   }
 }
 
+#ifndef _WIN32
 // See trackers #20988 and #42026
 TEST_F(LibRadosMisc, ShutdownRace)
 {
@@ -343,3 +346,4 @@ TEST_F(LibRadosMisc, ShutdownRace)
     threads[i].join();
   ASSERT_EQ(setrlimit(RLIMIT_NOFILE, &rold), 0);
 }
+#endif /* _WIN32 */
