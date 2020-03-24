17b2820c55069c74f2bf32c35bf0e9b3aa194dfd
Mike Latimer <mlatimer@suse.com>
2020-03-23 21:13:36 -0600

    Run win32 dependency build if libdokan.a doesn't exist
    At the moment, we assume that the dependencies have been successfully
    built if the "build.deps" directory exists.
    
    This change makes this check more robust by looking for libdokan.a,
    which is expected to be the last one to be built.
    
    Signed-off-by: Mike Latimer <mlatimer@suse.com>
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- win32_build.sh
+++ win32_build.sh
@@ -73,7 +73,9 @@ if [[ -n $CLEAN_BUILD ]]; then
     rm -rf $BUILD_DIR
 fi
 
-if [[ ! -d $DEPS_DIR ]]; then
+# libdokan is the last dependency to be retrieved. If libdokan.a
+# is available, we assume that all dependencies have been covered.
+if [[ ! -f ${dokanLibDir}/libdokan.a ]]; then
     echo "Preparing dependencies: $DEPS_DIR"
     NUM_WORKERS=$NUM_WORKERS DEPS_DIR=$DEPS_DIR \
         "$SCRIPT_DIR/win32_deps_build.sh"
