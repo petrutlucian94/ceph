f561f95e0bbfedb85c027bd73010902f0a0b2305
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2020-04-27 12:57:23 +0000

    squashme: errno fixes

--- src/common/errno.h
+++ src/common/errno.h
@@ -9,8 +9,8 @@ std::string cpp_strerror(int err);
 #ifdef _WIN32
 // While cpp_strerror handles errors defined in errno.h, this one
 // accepts standard Windows error codes.
-std::string win32_strerror(int err)
-std::string win32_lasterror_str()
+std::string win32_strerror(int err);
+std::string win32_lasterror_str();
 #endif /* _WIN32 */
 
 #endif
