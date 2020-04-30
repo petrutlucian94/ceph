1d5cebdbc656dca77723d039b10670a221427a0e
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2020-04-30 07:20:40 +0000

    common, compat: define DEV_NULL
    On Windows, the "nul" special path must be used instead of
    "/dev/null".
    
    Worth mentioning that reading from "nul" will report errors, unlike
    "/dev/null" on POSIX platforms. This doesn't affect writes.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- src/include/compat.h
+++ src/include/compat.h
@@ -350,6 +350,8 @@ extern _CRTIMP errno_t __cdecl _putenv_s(const char *_Name,const char *_Value);
 #define O_CLOEXEC 0
 #define SOCKOPT_VAL_TYPE char*
 
+#define DEV_NULL "nul"
+
 #else /* WIN32 */
 
 #define SOCKOPT_VAL_TYPE void*
@@ -359,6 +361,8 @@ extern _CRTIMP errno_t __cdecl _putenv_s(const char *_Name,const char *_Value);
 static inline int compat_closesocket(int fildes) {
   return close(fildes);
 }
+
+#define DEV_NULL "/dev/null"
 
 #endif /* WIN32 */
 
