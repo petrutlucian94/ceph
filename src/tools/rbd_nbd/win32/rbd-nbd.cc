007d9883e2c296724628d95c92f06672fd8a0f02
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2020-07-08 10:22:31 +0000

    rbd-nbd: accept FUA flag
    This change updates the RBD NBD server, propagating the Force
    Unit Flag (FUA) flag for write requests, ensuring that they are
    flushed.
    
    While the SCSI specs allow FUA to be used for read requests as well
    in order to ensure that read operations don't use stale data,
    NBD currently prohibits it.
    
    In order to avoid unnecessary operations, we'll accept FUA and flushes
    only when RBD writeback caching is enabled, advertising it via NBD
    flags.
    
    For now, we're only enabling FUA on Windows. If needed, the same can
    be done for Linux.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- src/tools/rbd_nbd/win32/rbd-nbd.cc
+++ src/tools/rbd_nbd/win32/rbd-nbd.cc
@@ -644,6 +644,9 @@ static int do_map(Config *cfg)
     return map_device_using_suprocess(GetCommandLine());
   }
 
+  bool rbd_cache = g_conf().get_val<bool>("rbd_cache");
+  std::string rbd_cache_policy = g_conf().get_val<std::string>("rbd_cache_policy");
+
   r = rados.init_with_context(g_ceph_context);
   if (r < 0)
     goto close_fd;
@@ -681,9 +684,14 @@ static int do_map(Config *cfg)
   if (r < 0)
     goto close_fd;
 
-  flags = NBD_FLAG_SEND_FLUSH | NBD_FLAG_SEND_TRIM | NBD_FLAG_HAS_FLAGS;
+  flags = NBD_FLAG_SEND_TRIM | NBD_FLAG_HAS_FLAGS;
   if (!cfg->snapname.empty() || cfg->readonly) {
     flags |= NBD_FLAG_READ_ONLY;
+  }
+  else if (rbd_cache && rbd_cache_policy == "writeback") {
+    // writearound and writeback shouldn't require flushes.
+    flags |= NBD_FLAG_SEND_FLUSH;
+    flags |= NBD_FLAG_SEND_FUA;
   }
 
   if (info.size > _UI64_MAX) {
