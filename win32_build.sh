bb0e3f3b2315508f0b570b0e3294736a2dff10e3
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2022-09-15 08:41:56 +0000

    win32_build.sh: build "ceph_test*"
    At the moment, win32_build.sh builds the "tests" target, which
    only includes the "unittest_*" tests.
    
    We'll update the build script to include "ceph_test_*" targets
    as well.
    
    Note that we can't just build all the targets that were generated
    by cmake since some of them are unavailable on Windows. When
    doing the Windows port, we decided to avoid polluting the Cmake
    files and keep the amount of Windows checks to a minimum.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- win32_build.sh
+++ win32_build.sh
@@ -198,6 +198,8 @@ if [[ -z $SKIP_BUILD ]]; then
     ninja_targets+=" compressor ceph_lz4 ceph_snappy ceph_zlib ceph_zstd"
     if [[ -z $SKIP_TESTS ]]; then
       ninja_targets+=" tests ceph_radosacl ceph_scratchtool"
+      ninja_targets+=" "
+      ninja_targets+=`ninja -t targets | grep ceph_test | cut -d ":" -f 1 | grep -v exe`
     fi
 
     ninja -v $ninja_targets 2>&1 | tee "${BUILD_DIR}/build.log"
