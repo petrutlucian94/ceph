36cabf71ce37e9bf2de1b420d2607e64431af5bd
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2023-05-04 15:13:37 +0000

    squash: print last tick

--- src/test/common/test_ceph_timer.cc
+++ src/test/common/test_ceph_timer.cc
@@ -167,7 +167,10 @@ template<typename TC>
 void tick(ceph::timer<TC>* t,
           typename TC::time_point deadline,
           double interval,
-          bool* test_finished) {
+          bool* test_finished,
+          typename TC::time_point* last_tick) {
+  *last_tick = TC::now();
+
   if (TC::now() > deadline) {
     *test_finished = true;
   } else {
@@ -181,6 +184,7 @@ TEST(TimerLoopTest, TimerLoop)
   bool test_finished = false;
   int test_duration = 30;
   double tick_interval = 0.00004;
+  auto last_tick = ceph::coarse_mono_clock::now();
 
   t.add_event(
     ceph::make_timespan(tick_interval),
@@ -188,10 +192,13 @@ TEST(TimerLoopTest, TimerLoop)
     &t,
     ceph::coarse_mono_clock::now() + std::chrono::seconds(test_duration),
     tick_interval,
-    &test_finished);
+    &test_finished,
+    &last_tick);
 
   std::this_thread::sleep_for(std::chrono::seconds(test_duration + 2));
 
   ASSERT_TRUE(test_finished)
-    << "The timer job didn't complete, it probably hanged.";
+    << "The timer job didn't complete, it probably hanged. "
+    << "Time since last tick: "
+    << (ceph::coarse_mono_clock::now() - last_tick);
 }
