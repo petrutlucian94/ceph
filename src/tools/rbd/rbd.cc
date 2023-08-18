2e50ca6b2f0a78b6483e5f86abadea46946fca1d
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2023-08-18 12:25:52 +0000

    common: Windows Unicode CLI support
    Windows CLI arguments use either ANSI (main()) or UTF-16 (wmain()).
    Meanwhile, Ceph libraries expect UTF-8 and raise exceptions when
    trying to use Unicode CLI arguments:
    
      rbd.exe create test_unicode_șțăâ --size=32M
      terminate called after throwing an instance of 'std::runtime_error'
        what():  invalid utf8
    
    We'll address this by converting the CLI arguments and output to
    UTF-8.
    
    By calling setlocale(LC_ALL, ".UTF8"), we enable Windows UCRT UTF-8
    mode so that functions that receive char* arguments will expect UTF-8
    instead of ANSI. One exception is CreateProcess, which will need the
    UTF-16 form (CreateProcessW).
    
    In order to use the UTF-8 locale, we'll have to switch the mingw-llvm
    runtime from msvcrt to ucrt.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- src/tools/rbd/rbd.cc
+++ src/tools/rbd/rbd.cc
@@ -3,8 +3,30 @@
 
 #include "tools/rbd/Shell.h"
 
+#ifdef _WIN32
+
+#include <iostream>
+
+#include "common/win32/wstring.h"
+
+int main(int argc, const char **argv)
+{
+  setlocale(LC_ALL, ".UTF8");
+  SetConsoleOutputCP(CP_UTF8);
+  char** argv_utf8 = get_utf8_argv();
+  if (!argv_utf8) {
+    std::cerr << "Couldn't convert args to utf8." << std::endl;
+    return -EINVAL;
+  }
+
+  rbd::Shell shell;
+  return shell.execute(argc, (const char**) argv_utf8);
+}
+
+#else
 int main(int argc, const char **argv)
 {
   rbd::Shell shell;
   return shell.execute(argc, argv);
 }
+#endif
